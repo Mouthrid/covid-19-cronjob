938b58e4964d6ba798e0e072cb047e6ef3814422
Ray <Ray_Xie@wistron.com>
2021-11-22 11:17:33 +0800

    chore: refine docker

--- docker/set_crontab.sh
+++ docker/set_crontab.sh
@@ -1,5 +1,9 @@
-HOST_IP=covid19-backend
-#APP_KEY=
-#youtube_api_key=
+cat << EOF > /etc/cron.d/crontab
+HOST_IP=${HOST_IP}
+APP_KEY=${APP_KEY}
+youtube_api_key=${youtube_api_key}
 00,20,40 05-10 * * * /usr/local/bin/python3.8 /app/src/main.py video >> /app/logs/video.log 2>&1
 00 16 * * * /usr/local/bin/python3.8 /app/src/main.py news >> /app/logs/news.log 2>&1
+EOF
+
+crontab /etc/cron.d/crontab
