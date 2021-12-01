7f6bba0bfcaf73d8e29835abe02c10ff31a35138
Ray Xie <birken1018@gmail.com>
2021-12-01 07:29:56 +0000

    fix(src): error limit causes news missing

--- src/news/main.py
+++ src/news/main.py
@@ -15,24 +15,20 @@ def main():
 
     logging.basicConfig(format='[%(asctime)s][%(levelname)s] %(message)s',
                         datefmt='%m/%d/%Y %H:%M:%S')
+    logging.getLogger().setLevel(logging.INFO)
+
     crawler = Crawler(prefix, endwith, pattern)
     uploader = Uploader(ip, os.getenv("APP_KEY"))
 
     for page in range(1, 2):
         for content in crawler.get_content(url.format(page=page), uploader.id_exists):
             if isinstance(content, tuple):
-                logging.info(f'POST {content}')
+                logging.info(f'POST: {content}')
                 uploader.post_news(News(*content).to_dict())
             elif isinstance(content, str):
-                uploader.error += 1
                 logging.warning(f'ID: {content} is existing in database')
             elif isinstance(content, KeyError):
-                uploader.error += 1
                 logging.error(f'KeyError: {content}')
-
-            if uploader.error == 10:
-                logging.warning('Too much reject, exiting now')
-                exit(0)
 
 
 if __name__ == '__main__':
